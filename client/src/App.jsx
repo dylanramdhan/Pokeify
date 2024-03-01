33e28c0bb5703618d8150ad8413870fb3c280f52
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-03-01 11:12:55 -0500

    Build: Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homePage';
+import LoginPage from './pages/LoginPage';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      This is Pokeify
-    </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route index element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
