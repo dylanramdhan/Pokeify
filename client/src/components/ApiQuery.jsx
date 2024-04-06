0297bf7b31757cb1265a48f6a43113848fe93d3d
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-04-06 19:17:23 -0400

    finished connecting both apis

--- client/src/components/ApiQuery.jsx
+++ client/src/components/ApiQuery.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react';
 import { pokemonQueryUrl, expressport } from '../utilities/global';
 
 
+
+
+
 const getPokemontoArtist = async (query) => {
     const res = await fetch(pokemonQueryUrl + query)
     if (res.ok) {
@@ -34,8 +37,9 @@ export default function ApiQuery() {
     const [query, setQuery] = useState('')
     const [artist, setArtist] = useState('')
     const [accessToken, setAccessToken] = useState('')
+    const [albums, setAlbums] = useState([])
 
-    const CLIENT_ID = ''
+    const CLIENT_ID = 'f18d386ae31746aea86f3eb2d9cdbad1'
     const CLIENT_SECRET = 'd38e056805f24b1e98a8aefa639ab94b'
 
     const handleChange = (e) => {
@@ -65,10 +69,40 @@ export default function ApiQuery() {
             const getArtist = await getPokemontoArtist(query)
             setArtist(getArtist)
             console.log(getArtist)
+            search()
         } catch (e) {
             console.error(e)
         }
     }
+
+    async function search() {
+        var searchParameters = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "authorization": "Bearer " + accessToken
+
+            }
+        }
+
+        var ArtistID = await fetch('https://api.spotify.com/v1/search?q=' + artist + '&type=artist', searchParameters)
+            .then(res => res.json())
+            .then(data => { return data.artists.items[0].id })
+
+        console.log(ArtistID)
+
+        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + ArtistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+            .then(res => res.json())
+            .then(
+                data => {
+                    console.log(data)
+                    setAlbums(data.items);
+
+                }
+            )
+
+    }
+
 
     useEffect(() => {
         getSpotifyAccessToken()
@@ -91,6 +125,14 @@ export default function ApiQuery() {
                 <TextField id="standard-basic" label="Standard" variant="standard" />
                 <Button type="submit" variant="contained" >Text</Button>
             </Box>
+
+            {albums.map(album => {
+                return <div key={album.id}>
+                    <h1>{album.name}</h1>
+                    <img src={album.images[0].url} alt={album.name} />
+                </div>
+            })
+            }
 
         </div>
     )
