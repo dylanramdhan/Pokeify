4cdf54efc37bdc0ee9c24fab1e592c55bd419ee2
Wai Lun Mak <angusmak@bu.edu>
2024-04-11 22:54:31 -0400

    padding

--- client/src/pages/userPage.jsx
+++ client/src/pages/userPage.jsx
@@ -2,8 +2,13 @@ import ApiQuery from "../components/ApiQuery"
 
 export default function UserPage() {
     return (
-        <div className='mx-10 my-10'>
-            <h1 className='text-3xl font-bold text-center mb-8'>Type in a Pokemon Name to see its Music equivalent </h1>
+        <div className='mx-20 my-20' class="bg-red-200">
+            <img
+                    src="https://www.freepnglogos.com/uploads/pok-mon-go-logo-png-30.png"  
+                    alt="Spotify Logo"
+                    className="mx-auto border-t-6 mb-2 object-scale-down h-20 w-20"
+                />
+            <h1 className='text-3xl font-bold text-center mb-6'>Type in a Pokémon name to see its music equivalent!</h1>
             <ApiQuery />
         </div>
     )
