d47a115abf1e047aea6df373296e35ece29537d2
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-04-17 22:24:28 -0400

    finished connecting database

--- server/app.js
+++ server/app.js
@@ -2,13 +2,55 @@ const express = require("express");
 const cors = require('cors');
 const { pokemonTypes, pokemonRap, genreToArtists } = require('./global.js')
 require('dotenv').config();
+const { User } = require('./schema/user.js');
 
+
+const mongoose = require('mongoose');
 const app = express();
+
 app.use(cors());
-// Middleware to parse JSON bodies
 app.use(express.json({ limit: '10mb' }));
-// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+}
+connectToMongo();
+
+
+
+'---'
+
+app.post('/database/postUser', async (req, res) => {
+  try {
+    const user = req.body;
+    console.log('Received user information:', user);
+
+    // Check if the user with the provided email already exists
+    const existingUser = await User.findOne({ email: user.email });
+    if (existingUser) {
+      // If user already exists, send a conflict response
+      console.log('User with email already exists');
+      return res.status(409).json({ error: 'User with email already exists' });
+    }
+
+    // If user doesn't exist, create a new user
+    await User.create(user);
+
+    console.log('User created successfully')
+    res.status(201).json({ message: 'User created successfully' });
+
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 app.post("/api/pokemonQuery", function async(req, res) {
---
+++ server/schema/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    picture: String
+});
+
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = { User };
